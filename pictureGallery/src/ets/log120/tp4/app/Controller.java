cc7fd47e036e3372e68759615fb00d2320e23d9e
authchir <martin.desharnais@gmail.com>
2011-11-30 09:08:50 -0500

    Ajoute la classe Controller qui est responsable de conserver les commandes effectués par l'utilisateur et offre les fonction « défaire » et « refaire ».

---
+++ pictureGallery/src/ets/log120/tp4/app/Controller.java
@@ -0,0 +1,54 @@
+package ets.log120.tp4.app;
+
+public class Controller {
+	
+	// --------------------------------------------------
+	// Constructeur(s)
+	// --------------------------------------------------
+	
+	public Controller() {
+		pastCommands = new java.util.LinkedList<Command>();
+		futurCommands = new java.util.LinkedList<Command>();
+	}
+	
+	// --------------------------------------------------
+	// Accesseur(s)
+	// --------------------------------------------------
+
+	// --------------------------------------------------
+	// Mutateur(s)
+	// --------------------------------------------------
+	
+	public void performCommand(Command command) {
+		command.doCommand();
+		pastCommands.addLast(command);
+		futurCommands.clear();
+	}
+	
+	public void undo() {
+		if (pastCommands.size() > 0) {
+			Command command = pastCommands.removeLast();
+			command.undoCommand();
+			futurCommands.addLast(command);
+		}
+	}
+	
+	public void redo() {
+		if (futurCommands.size() > 0) {
+			Command command = futurCommands.removeLast();
+			command.doCommand();
+			pastCommands.addLast(command);
+		}
+	}
+	
+	// --------------------------------------------------
+	// Méthode(s)
+	// --------------------------------------------------
+	
+	// --------------------------------------------------
+	// Attribut(s)
+	// --------------------------------------------------
+	
+	java.util.LinkedList<Command> pastCommands;
+	java.util.LinkedList<Command> futurCommands;
+}
