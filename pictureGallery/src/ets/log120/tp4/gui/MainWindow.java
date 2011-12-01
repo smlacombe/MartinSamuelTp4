d6ca3e25da4154d6a1757ea5865575a8726ee441
authchir <martin.desharnais@gmail.com>
2011-11-30 22:22:42 -0500

    Ajout d'une interface graphique de base pour tester l'interraction entre les classes.

--- pictureGallery/src/ets/log120/tp4/gui/MainWindow.java
+++ pictureGallery/src/ets/log120/tp4/gui/MainWindow.java
@@ -1,10 +1,22 @@
 package ets.log120.tp4.gui;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Observable;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+import ets.log120.tp4.app.Controller;
+import ets.log120.tp4.app.Perspective;
+import ets.log120.tp4.app.PerspectiveFactory;
+import ets.log120.tp4.app.ZoomCommand;
+import ets.log120.tp4.app.TranslationCommand;
 
 public class MainWindow extends JFrame {
 	
@@ -15,10 +27,64 @@ public class MainWindow extends JFrame {
 	public MainWindow() {
 		super();
 		initLang();
+		setLayout(new FlowLayout());
+		
+		add(imageView = new JTextArea(5, 20));
+		imageView.setEditable(false);
+		
+		add(button1 = new JButton("Image"));
+		button1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				image.setImage("image2.png");
+			}
+		});
+		
+		add(button2 = new JButton("Zoom"));
+		button2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				controller.performCommand(new ZoomCommand(image, 0.5));
+			}
+		});
+		
+		add(button3 = new JButton("Position"));
+		button3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				controller.performCommand(new TranslationCommand(image, 10, 10));
+			}
+		});
+		
+		add(undoButton = new JButton("Annuler"));
+		undoButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				controller.undo();
+			}
+		});
+		
+		add(redoButton = new JButton("Refaire"));
+		redoButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				controller.redo();
+			}
+		});
+		
+		validate();
+		
+		PerpectiveChanged listener = new PerpectiveChanged();
+		image = PerspectiveFactory.makePerspective();
+		image.imageChanged.addObserver(listener);
+		image.zoomChanged.addObserver(listener);
+		image.positionChanged.addObserver(listener);
+		image.setImage("image.png");
 		
 		setTitle(lang.getProperty("app.title"));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(getPreferredSize());
+		setSize(500, 500);
+		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 	
@@ -48,4 +114,26 @@ public class MainWindow extends JFrame {
 	// --------------------------------------------------
 	
 	private java.util.Properties lang;
+	
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private JButton undoButton;
+	private JButton redoButton;
+	private JTextArea imageView;
+	private Perspective image;
+	private Controller controller = new Controller();;
+	
+	// --------------------------------------------------
+	// Classe(s) interne(s)
+	// --------------------------------------------------
+	
+	private class PerpectiveChanged implements java.util.Observer {
+		@Override
+		public void update(Observable arg0, Object arg1) {
+			imageView.setText("Image: " + image.getImage()
+				+ "\nZoom: " + image.getZoom()
+				+ "\nPosition: (" + image.getPosition().getX() + ", " + image.getPosition().getX() + ")");
+		}
+	}
 }
