7a089dabb1e78ef900b24ed64cb610f517a08b9e
authchir <martin.desharnais@gmail.com>
2011-12-13 10:25:31 -0500

    Refactoring de la classe TextualPerspectiveView.

--- pictureGallery/src/ets/log120/tp4/gui/Main.java
+++ pictureGallery/src/ets/log120/tp4/gui/Main.java
@@ -1,7 +1,5 @@
 package ets.log120.tp4.gui;
 
-import java.io.IOException;
-
 public class Main {
 	public static void main(String[] args) {
 		MainWindow app = new MainWindow();
--- pictureGallery/src/ets/log120/tp4/gui/TextualPerspectiveView.java
+++ pictureGallery/src/ets/log120/tp4/gui/TextualPerspectiveView.java
@@ -1,53 +1,44 @@
 package ets.log120.tp4.gui;
 
-import java.awt.Color;
 import java.util.Observable;
 
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import ets.log120.tp4.app.Perspective;
-import ets.log120.tp4.app.PerspectiveFactory;
 
 public class TextualPerspectiveView extends JPanel {
 	
 	public TextualPerspectiveView(Perspective perspective) {
-		this.perspective = perspective;
-		initView();		
-	}
-	
-	private void initView() {
-		initJTextArea();
-		add (textView);
+		add(textView = getJTextArea());
 		
 		PerpectiveChanged listener = new PerpectiveChanged();
 		perspective.imageChanged.addObserver(listener);
 		perspective.zoomChanged.addObserver(listener);
 		perspective.positionChanged.addObserver(listener);
 		
-		updateText();
+		updateText(perspective);
 	}
 	
-	private void initJTextArea() {
-		textView = new JTextArea(5, 20);
-		textView.setEditable(false);
-		textView.setLineWrap(true);
+	private JTextArea getJTextArea() {
+		JTextArea area = new JTextArea(5, 20);
+		area.setEditable(false);
+		area.setLineWrap(true);
+		return area;
 	}
 	
-	private void updateText() {
-		textView.setText("Image: " + perspective.getImageName()
-				+ "\nZoom: " + Math.round(perspective.getZoom()*100) + "%"
-				+ "\nPosition: (" + perspective.getPosition().getX() + ", " + perspective.getPosition().getY() + ")");
+	private void updateText(Perspective p) {
+		textView.setText("Image: " + p.getImageName()
+				+ "\nZoom: " + Math.round(p.getZoom()*100) + "%"
+				+ "\nPosition: (" + p.getPosition().getX() + ", " + p.getPosition().getY() + ")");
 	}
-		
 	
 	private JTextArea textView;
-	private Perspective perspective;
 	
 	private class PerpectiveChanged implements java.util.Observer {
 		@Override
 		public void update(Observable arg0, Object arg1) {
-			updateText();
+			updateText((Perspective) arg1);
 		}
 	}
 }
